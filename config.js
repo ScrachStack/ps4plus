51b3758a77c39220b24180ab17431b9818e24cb3
Safauri <61665278+Safauri@users.noreply.github.com>
2025-10-19 19:53:57 -0400

    Add files via upload

--- config.js
+++ config.js
@@ -1,9 +1,4 @@
-const config = {
-    token: "TOKEN",
-    guildIds: ['1427330535835041794', ], // You can put multiple IDs here
- 
-  owner_id: "",
-  
-  module.exports = config;
-  
-  
+module.exports = {
+  token: "TOKEN",
+  clientId: "BOT ID"
+};
